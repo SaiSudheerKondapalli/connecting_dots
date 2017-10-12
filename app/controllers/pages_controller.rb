4a4f7c0a65df10c706b4fd65e44f3427c3e3ba75
Sudheer <saisudheerkondapalli@hotmail.com>
2017-10-13 00:36:00 +0530

    Strong Parameters and adding new col user name in reg page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,16 @@ class PagesController < ApplicationController
   def home
   end
 
-  def profile
-  end
+  # back-end code for pages/profile
+    def profile
+      # grab the username from the URL as :id
+      if (User.find_by_username(params[:id]))
+        @username = params[:id]
+      else
+        # redirect to 404 (root for now)
+        redirect_to root_path, :notice=> "User not found!"
+      end
+    end
 
   def index
   end
